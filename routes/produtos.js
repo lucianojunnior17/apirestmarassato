b50f0c153bbea49a641d10cd3281e11f86ac6ac8
Luciano Junior <engluciano.junior@gmail.com>
2021-01-24 01:41:04 -0300

    aula03

---
+++ routes/produtos.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { router } = require('../app');
+const routes = express.Router();
+
+routes.get('/', (req, res, next) => {
+    res.status(200).send({
+        message: 'Usando GET dentro da rotas de produtos'
+    });
+});
+
+routes.post('/', (req, res, next) => {
+    res.status(201).send({
+        message: 'Usando rota POST dentro  de produtos'
+    });
+});
+
+router.get('/:id_produto', (req, res, next) => {
+    const id = req.params.id_produto;
+    res.status(200).send({
+        message: 'Usando GET de Um produto especifico',
+        id: id
+    });
+});
+
+module.exports = routes;
